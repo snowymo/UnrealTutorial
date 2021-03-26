acca617d9b142e9e0d40a91ac190ede451a8ad00
Zhenyi He <snowymohehe@gmail.com>
2021-03-25 21:57:12 -0400

    :tada: init tcp test

---
+++ Tcp101/Source/Tcp101/Tcp101GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tcp101GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TCP101_API ATcp101GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
